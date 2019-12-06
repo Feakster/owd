2868876e4c08c4e079e51bc5f869a192ccbba926
Benjamin Feakins <greatwhite@posteo.eu>
2019-12-06 10:24:26 +0000

    Faffing

--- owd.R
+++ owd.R
@@ -4,10 +4,19 @@
 #                   #
 #===================#
 
-install.packages(c(
-  "devtools",
-  "usethis",
-  "roxygen2",
-  "testthat",
-  "git2r"
-))
+# Hidden Environment #
+.env <- new.env()
+
+# Open File Manager to Current Directory #
+.env$o <- function(...){
+  if(Sys.info()['sysname'] == 'Linux'){
+    system('nautilus .', ignore.stderr = T, wait = F)
+  }
+  if(Sys.info()['sysname'] == 'Darwin'){
+    system('open .', wait = F)
+  }
+  if(Sys.info()['sysname'] == 'Windows'){
+    system('start .', wait = F)
+  }
+}
+
