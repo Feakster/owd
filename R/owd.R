282e67c04f70cc512924ccec0307f7257e7a553a
Feakster <greatwhite@posteo.eu>
2019-12-16 14:26:18 +0000

    IMPROVED LINUX FLEXIBILITY
    - Added the ability of the function to test for a series of possible Linux file managers. The 'best' candidate is selected as the one occurring first in the system $PATH. Ties are resolved using the initial order provided.
    - Restructured the function as an if else ladder.
    - Added stop clauses and error messages for unsupported operating systems or file managers.
    - Removed the ability to specify arguments. I may add to ability to specify a directory path in future.
    - May add BSD support in future.
    - May add more Linux file managers in future.

--- R/owd.R
+++ R/owd.R
@@ -8,14 +8,28 @@
 #' @export
 #'
 
-owd <- function(...){
-  if(Sys.info()['sysname'] == 'Linux'){
-    system('nautilus .', ignore.stderr = T, wait = F)
-  }
-  if(Sys.info()['sysname'] == 'Darwin'){
-    system('open .', wait = F)
-  }
-  if(Sys.info()['sysname'] == 'Windows'){
-    system('start .', wait = F)
+### owd() ###
+owd <- function(){
+  if(Sys.info()[['sysname']] == 'Linux'){
+    ## Linux ##
+    # Extract System $PATH #
+    Sys.path <- system('echo $PATH', intern = T)
+    Sys.path <- strsplit(Sys.path, split = ":")[[1]]
+    # Possible File Managers #
+    file.man <- c('nautilus', 'konqueror', 'dolphin', 'pcmanfm', 'nemo', 'thunar', 'krusader')
+    # File Managers Present #
+    file.man <- file.man[sapply(file.man, function(x){!system(paste('which', x), ignore.stdout = T, ignore.stderr = T)})]
+    if (length(file.man) == 0) stop('File manager not supported!')
+    # File Manager Preferred in $PATH (Ties Resolved as Order Above) #
+    file.man <- file.man[which.min(sapply(file.man, function(x){which.max(file_test("-f", file.path(Sys.path, x)))}))]
+    system(paste(file.man, "."), ignore.stdout = T, ignore.stderr = T, wait = F)
+  } else if(Sys.info()[['sysname']] == 'Darwin'){
+    ## macOS ##
+    system('open .', ignore.stdout = T, ignore.stderr = T, wait = F)
+  } else if(Sys.info()[['sysname']] == 'Windows'){
+    ## Windows ##
+    system('start .', ignore.stdout = T, ignore.stderr = T, wait = F)
+  } else {
+    stop('Operating system not supported!')
   }
 }
