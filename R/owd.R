50f94c8618d6fbb45af7232aba565e0aa87e5316
Feakster <greatwhite@posteo.eu>
2019-12-16 14:28:10 +0000

    Faffing.

--- R/owd.R
+++ R/owd.R
@@ -19,7 +19,7 @@ owd <- function(){
     file.man <- c('nautilus', 'konqueror', 'dolphin', 'pcmanfm', 'nemo', 'thunar', 'krusader')
     # File Managers Present #
     file.man <- file.man[sapply(file.man, function(x){!system(paste('which', x), ignore.stdout = T, ignore.stderr = T)})]
-    if (length(file.man) == 0) stop('File manager not supported!')
+    if(length(file.man) == 0) stop('File manager not supported!')
     # File Manager Preferred in $PATH (Ties Resolved as Order Above) #
     file.man <- file.man[which.min(sapply(file.man, function(x){which.max(file_test("-f", file.path(Sys.path, x)))}))]
     system(paste(file.man, "."), ignore.stdout = T, ignore.stderr = T, wait = F)
