df33f7c791fed1ba6f23b58dbf2d5820f9212629
Feakster <greatwhite@posteo.eu>
2019-12-31 11:46:34 +0000

    - Reduced the number of direct system() calls in favour of R's native system interface functions, such as Sys.getenv() and Sys.which(). - Syntax tidying.

--- R/owd.R
+++ R/owd.R
@@ -13,14 +13,14 @@ owd <- function(){
   if(Sys.info()[['sysname']] == 'Linux'){
     ## Linux ##
     # Extract System $PATH #
-    Sys.path <- system('echo $PATH', intern = T)
-    Sys.path <- strsplit(Sys.path, split = ":")[[1]]
+    Sys.path <- Sys.getenv()[['PATH']]
+    Sys.path <- unlist(strsplit(Sys.path, split = ":"))
     # Possible File Managers #
     file.man <- c('nautilus', 'konqueror', 'dolphin', 'pcmanfm', 'nemo', 'thunar', 'krusader')
     # File Managers Present #
-    file.man <- file.man[sapply(file.man, function(x){!system(paste('which', x), ignore.stdout = T, ignore.stderr = T)})]
+    file.man <- file.man[sapply(file.man, function(x){Sys.which(x) != ''})]
     if(length(file.man) == 0) stop('File manager not supported!')
-    # File Manager Preferred in $PATH (Ties Resolved as Order Above) #
+    # File Manager Preferred in $PATH (Ties Resolved as Initial Order Above) #
     file.man <- file.man[which.min(sapply(file.man, function(x){which.max(file_test("-f", file.path(Sys.path, x)))}))]
     system(paste(file.man, "."), ignore.stdout = T, ignore.stderr = T, wait = F)
   } else if(Sys.info()[['sysname']] == 'Darwin'){
@@ -28,7 +28,7 @@ owd <- function(){
     system('open .', ignore.stdout = T, ignore.stderr = T, wait = F)
   } else if(Sys.info()[['sysname']] == 'Windows'){
     ## Windows ##
-    shell.exec(".")
+    shell.exec('.')
   } else {
     stop('Operating system not supported!')
   }
